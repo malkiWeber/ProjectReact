9ba2ceb4bb78ccbe28668d460edbfa3067a1bab4
וואקנין מיכל < @mbjcomp.local>
2025-01-28 19:48:30 +0200

    mychange

--- שיעור 1/my-react-app/src/components/ProfileCard.jsx
+++ שיעור 1/my-react-app/src/components/ProfileCard.jsx
@@ -2,7 +2,9 @@ const Profilecard = (prop) => {
 
     return (
         <>
-            <h1>Hello: {prop.name} your age: {prop.age} hobby; {prop.hobby}</h1>
+            <h1>Hello: {prop.name}What is  your age? {prop.age} hobby; {prop.hobby}</h1>
+            <h2>MICHAL&TAMI</h2>
+
             
         </>
     )
